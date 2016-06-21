711ca3d001261b34780473ab8e73a55e3cf8a880
MishaHerscu <mishaherscu@gmail.com>
2016-06-21 09:35:05 -0400

    warmup

--- grandma.rb
+++ grandma.rb
@@ -1,1 +1,16 @@
 require 'pry'
+
+def talk
+  txt = gets
+  txt == txt.upcase ? m = 'NO, NOT SINCE 1938!' : m = 'HUH?! SPEAK UP, SONNY!'
+  `say "#{m}"`
+  if txt == 'BYE'
+    m = 'BYE!'
+    `say "#{m}"`
+    return
+  else
+    talk
+  end
+end
+
+talk
